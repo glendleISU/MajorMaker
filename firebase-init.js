b27af0ef4cb4be80a3e66daaff6b0cbce1108d3f
glendleISU <145153080+glendleISU@users.noreply.github.com>
2025-05-30 15:35:18 -0500

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDudaFY9ZkQthsOPkn5MKCWUV4GdJ__YEI",
+  authDomain: "major-maker-580e8.firebaseapp.com",
+  databaseURL: "https://major-maker-580e8-default-rtdb.firebaseio.com",
+  projectId: "major-maker-580e8",
+  storageBucket: "major-maker-580e8.firebasestorage.app",
+  messagingSenderId: "342221542780",
+  appId: "1:342221542780:web:057da75b81136edd4a40f9",
+  measurementId: "G-LGWRJMMMSS"
+};
+firebase.initializeApp(firebaseConfig);
